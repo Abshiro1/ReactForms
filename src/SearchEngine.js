217a2176cccbd731423cda79fa42f6f293ef8e1b
Abshiro1 <149562507+Abshiro1@users.noreply.github.com>
2024-05-29 20:07:46 +0200

    Initial commit

---
+++ src/SearchEngine.js
@@ -0,0 +1,19 @@
+import react, { useState } from "react";
+
+export default function SearchEngine() {
+  const [city, setCity] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${city}...`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
